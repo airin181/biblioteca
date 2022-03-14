59dfb26ad815b6c1e4e5049f769f2f08df60b310
airin181 <98608110+airin181@users.noreply.github.com>
2022-03-14 19:26:35 +0100

    read more button

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 // API KEY: VEv4zKkH6mydKKCzo97zoOhuO0w0801c
-
 // URL: https://api.nytimes.com/svc/books/v3/lists/names.json?api-key=VEv4zKkH6mydKKCzo97zoOhuO0w0801c
 
 const apiKey = 'VEv4zKkH6mydKKCzo97zoOhuO0w0801c';
@@ -21,50 +20,77 @@ allLists()
 
     for (let i = 0; i < list.length; i++) {
 
-//Nombre completo de la lista
-    const titleH3 = document.createElement("h3");
-//Fecha del libro más antiguo en la lista
-    const oldestP = document.createElement("p");
-//Fecha del último libro incorporado
-    const newestP = document.createElement("p");
-//Frecuencia de actualización
-    const updatingP = document.createElement("p");
-//Link para poder cargar la lista
-    const readMore = document.createElement("button");
+    //Nombre completo de la lista
+        const titleH3 = document.createElement("h3");
+    //Fecha del libro más antiguo en la lista
+        const oldestP = document.createElement("p");
+    //Fecha del último libro incorporado
+        const newestP = document.createElement("p");
+    //Frecuencia de actualización
+        const updatingP = document.createElement("p");
+    //Link para poder cargar la lista
+        const readMore = document.createElement("button");
 
-//div 1
-const category1 = document.createElement("div");
-//div 2
-const category2 = document.createElement("div")
+    //div 1
+    const category1 = document.createElement("div");
+    //div 2
+    const category2 = document.createElement("div")
 
-category1.appendChild(titleH3);
-category2.append(oldestP, newestP, updatingP, readMore);
-category1.append(category2);
+    category1.appendChild(titleH3);
+    category2.append(oldestP, newestP, updatingP, readMore);
+    category1.append(category2);
 
-category2.setAttribute("class", "category2")
-category1.setAttribute("class", "category1")
+    category2.setAttribute("class", "category2")
+    category1.setAttribute("class", "category1")
 
-const dashBoard = document.getElementById("dashboard");
-dashBoard.appendChild(category1);
+    const dashBoard = document.getElementById("dashboard");
+    dashBoard.appendChild(category1);
 
-//PINTAR EN HTML:
-titleH3.innerHTML = `${list[i].list_name}`;
-oldestP.innerHTML = `<b>Oldest published date:</b> ${list[i].oldest_published_date}`;
-newestP.innerHTML = `<b>Newest published date:</b> ${list[i].newest_published_date}`;
-updatingP.innerHTML = `<b>Updated:</b> ${list[i].updated}`;
+    //PINTAR EN HTML:
+    titleH3.innerHTML = `${list[i].list_name}`;
+    oldestP.innerHTML = `<b>Oldest published date:</b> ${list[i].oldest_published_date}`;
+    newestP.innerHTML = `<b>Newest published date:</b> ${list[i].newest_published_date}`;
+    updatingP.innerHTML = `<b>Updated:</b> ${list[i].updated}`;
 
-readMore.innerHTML = "Read More here";
+    readMore.innerHTML = "Read More here";
 
-    }          
+    readMore.setAttribute("class", "readMore");
 
+    
 
+        document.getElementsByClassName("readMore")[i].addEventListener("click", (event) => {
+        
+            async function booksCategory() {
+                try {
+                    const response = await fetch(`https://api.nytimes.com/svc/books/v3/lists/${list[i].list_name_encoded}.json?api-key=${apiKey}`);
+                    const data = await response.json();
+                    return data
+                } catch (error) {
+                    console.error(error);
+                }
+            }
+            booksCategory()
+            .then(function(){
+                
+              //pintar
+                
+                
+                
+                `${list[i].list_name_encoded}`
+            
             })
+        })
+   
 
-            /*
-"list_name": "Combined Print and E-Book Fiction",
-"display_name": "Combined Print & E-Book Fiction",
-"list_name_encoded": "combined-print-and-e-book-fiction",
-"oldest_published_date": "2011-02-13",
-"newest_published_date": "2022-03-20",
-"updated": "WEEKLY"
-*/
+// https://api.nytimes.com/svc/books/v3/lists/overview.json?api-key=VEv4zKkH6mydKKCzo97zoOhuO0w0801c
+
+
+
+
+    }  
+})
+
+
+
+
+
