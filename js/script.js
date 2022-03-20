7142979b0964f53cf2a3f97e7da2d39fb1bdc1df
airin181 <98608110+airin181@users.noreply.github.com>
2022-03-20 11:46:41 +0100

    UPDATE: css + ADD: Amazon & back btn

--- js/script.js
+++ js/script.js
@@ -1,10 +1,15 @@
-// API KEY: VEv4zKkH6mydKKCzo97zoOhuO0w0801c
 // URL: https://api.nytimes.com/svc/books/v3/lists/names.json?api-key=VEv4zKkH6mydKKCzo97zoOhuO0w0801c
 
-const apiKey = 'VEv4zKkH6mydKKCzo97zoOhuO0w0801c';
+const apiKey = "VEv4zKkH6mydKKCzo97zoOhuO0w0801c";
 const dashBoard = document.getElementById("dashboard");
+const dashBoard2 = document.getElementById("dashboard2");
+let category1 = document.querySelectorAll(".category1")
 let list = []
 let info = []
+
+const title = document.createElement("span")
+    title.innerHTML = `<h2 id="title">Welcome to <b>Books 4You</b></h2><p>A service dedicated to students of the University of London's Distance learning community. We provide online resources, professional support and guidance to all our students whenever, and from wherever they have chosen to study.</p><figure><img src="./media/bg.jpg" id="banner"></figure><p>Check all the best book categories we have selected for you</p><div id="title2-div"><img src="https://img.icons8.com/material-rounded/48/000000/chevron-down.png" id="arrow-title"/><h2 id="title2">Book categories</h2></div>`;
+    dashBoard.appendChild(title);
 
 
 async function allLists() {
@@ -22,17 +27,19 @@ function remove(element){
 }
 
 function firstLoop() {
+    
+
     for (let i = 0; i < list.length; i++) {
     //Nombre completo de la lista
-        const h3 = document.createElement("h3");
+    const h3 = document.createElement("h3");
     //Fecha del libro más antiguo en la lista
-        const p1 = document.createElement("p");
+    const p1 = document.createElement("p");
     //Fecha del último libro incorporado
-        const p2 = document.createElement("p");
+    const p2 = document.createElement("p");
     //Frecuencia de actualización
-        const p3 = document.createElement("p");
+    const p3 = document.createElement("p");
     //Link para poder cargar la lista
-        const read = document.createElement("button");
+    const read = document.createElement("button");
     //div 1
     const category1 = document.createElement("div");
     //div 2
@@ -60,7 +67,6 @@ function firstLoop() {
         await booksCategory(i);
         await secondLoop();
     })
-
 }
 }
 
@@ -77,7 +83,10 @@ async function booksCategory(i) {
 }
 
 function secondLoop(){
-    
+    const back = document.createElement("span")
+    back.innerHTML = `<input type="button" value="< GO BACK" onclick="back()" id="back"/>`;
+    dashBoard2.appendChild(back)
+
     for (let i = 0; i < info.length; i++) {
         
     //Portada
@@ -99,7 +108,6 @@ function secondLoop(){
     const divButton = document.createElement("div");
 
     //append
-
     divTitle.appendChild(h3);
     divImg.appendChild(poster)
     divInfo.append(p1, p2);
@@ -112,16 +120,15 @@ function secondLoop(){
     divInfo.setAttribute("class", "divInfo");
     divImg.setAttribute("class", "divImg");
     container.setAttribute("class", "container");
-    /* infoImg.setAttribute("class", "infoImg") */
+    
     divButton.setAttribute("class", "divButton");
-
 
 
     //PINTAR EN HTML:
     poster.innerHTML = `<img src=${info[i].book_image} class="poster">`;
     h3.innerHTML = `${info[i].title}`;
-    p1.innerHTML = `<b>Weeks on list:</b> ${info[i].weeks_on_list}`;
-    p2.innerHTML = `<b>Description:</b> ${info[i].description}`;
+    p1.innerHTML = `<u>Weeks on list:</u> ${info[i].weeks_on_list}`;
+    p2.innerHTML = `<u>Description:</u> ${info[i].description}`;
     amazon.innerHTML = "Purchase now!";     
         
 
@@ -134,15 +141,28 @@ function secondLoop(){
 
 const library = async () => {
     await allLists();
-    await firstLoop();
+    firstLoop();
 }
 
 
+function back(){
+    if(info = true){
+        window.location.href="./index.html"
+    }
+} 
 
 //______________<<<<<<<<>>>>>>>_______________
 
+title.addEventListener("click", (event) => {
+    if(list==false){
+        library();
+        document.getElementById("arrow-title").style.transform="rotate(180deg)"
+    } else if (list!==[""]){
+        back();
+    }
+    
+})
 
-library();
 
 
 
